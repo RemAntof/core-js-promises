863dff5b74c9e111f95e0d2a1937e143cb3eee3b
Anton Ramarchuk <anton.ramarchuk@sahaty.local>
2024-04-13 18:17:04 +0200

    feat: add solution 3

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -64,8 +64,16 @@ function getPromiseResult(source) {
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with 1
  * [Promise.reject(1), Promise.reject(2), Promise.reject(3)]    => Promise rejected
  */
-function getFirstResolvedPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstResolvedPromiseResult(promises) {
+  return new Promise((resolve) => {
+    Promise.any(promises)
+      .then((val) => {
+        resolve(val);
+      })
+      .catch(() => {
+        resolve('Promise rejected');
+      });
+  });
 }
 
 /**
