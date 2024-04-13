37cb7322c34f6e86a6460ae84a8802de58cf8cd3
Anton Ramarchuk <anton.ramarchuk@sahaty.local>
2024-04-13 18:19:17 +0200

    feat: add solution 4 and 5

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -95,8 +95,16 @@ function getFirstResolvedPromiseResult(promises) {
  * [promise3, promise6, promise2] => Promise rejected with 2
  * [promise3, promise4, promise6] => Promise rejected with 6
  */
-function getFirstPromiseResult(/* promises */) {
-  throw new Error('Not implemented');
+function getFirstPromiseResult(promises) {
+  return new Promise((resolve, reject) => {
+    Promise.race(promises)
+      .then((val) => {
+        resolve(val);
+      })
+      .catch((val) => {
+        reject(val);
+      });
+  });
 }
 
 /**
@@ -110,8 +118,16 @@ function getFirstPromiseResult(/* promises */) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  return new Promise((resolve, reject) => {
+    Promise.all(promises)
+      .then((val) => {
+        resolve(val);
+      })
+      .catch((val) => {
+        reject(val);
+      });
+  });
 }
 
 /**
