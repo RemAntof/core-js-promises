fa2491fe235c7866b8eb961d738b186701b488ef
Anton Ramarchuk <anton.ramarchuk@sahaty.local>
2024-04-13 18:47:42 +0200

    feat: add solution task 7

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -166,8 +166,19 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  let result = '';
+  return promises
+    .reduce((previousPromise, nextPromise) => {
+      return previousPromise.then((value) => {
+        result += value;
+        return nextPromise;
+      });
+    })
+    .then((lastValue) => {
+      result += lastValue;
+      return result;
+    });
 }
 
 module.exports = {
